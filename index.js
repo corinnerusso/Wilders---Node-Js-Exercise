6cd898b66800d8c9a38fb71092dcba8081fbdcd7
Corinne Russo <corinne.russo@gmail.com>
2019-10-02 10:38:44 +0200

    commit de l'index

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.bgCyan.red('Hello David!'));
+console.log(chalk.red('Hello Kevin!'));
+console.log(chalk.yellow('Hello Staphanie!'));
+console.log(chalk.bgWhiteBright.greenBright('Hello Chloé!'));
